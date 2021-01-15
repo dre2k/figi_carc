1decd2d6d0a3782f8fd01d5673b245b8936d6023
andreeki <andreeki@discovery1.usc.edu>
2021-01-15 15:52:53 -0800

    01/15/2020 working version

--- plink_ld_clump_plots/test.sh
+++ plink_ld_clump_plots/test.sh
@@ -2,13 +2,15 @@
 #SBATCH --time=23:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=8GB
-#SBATCH --account=lc_dvc
-#SBATCH --partition=conti
-#SBATCH --constraint=IB
+#SBATCH --account=dconti_251
 
-wdir=/scratch/andreeki/gwis/clump_combined
-exposure=pure_ep_allNo
-hrc_version=v2.3
+module load gcc/8.3.0
+module load openblas/0.3.8
+module load r/4.0.0
+
+wdir=/scratch/andreeki/temporary
+exposure=calcium_dietqc2
+hrc_version=v3.0
 #mkdir -p ${out_dir}
 
 # main effects p value clump
@@ -19,25 +21,32 @@ hrc_version=v2.3
 #    --clump-p1 0.0001 --clump-r2 0.2 --clump-p2 1 \
 #    --out   ${wdir}/figi_gxescan_${exposure}_chiSqG_ldclump
 
-# E|G p value clump
+
+plink \
+    --bfile /scratch/andreeki/clump/figi_controls_1000 \
+    --memory 8000 \
+    --clump ${wdir}/figi_gxescan_${exposure}_chiSqGxE_ldclump.txt \
+    --clump-p1 0.0001 --clump-r2 0.2 --clump-p2 1 \
+    --out   ${wdir}/figi_gxescan_${exposure}_chiSqGxE_ldclump
+
+plink \
+    --bfile /scratch/andreeki/clump/figi_controls_1000 \
+    --memory 8000 \
+    --clump ${wdir}/figi_gxescan_${exposure}_chiSqControl_ldclump.txt \
+    --clump-p1 0.0001 --clump-r2 0.2 --clump-p2 1 \
+    --out   ${wdir}/figi_gxescan_${exposure}_chiSqControl_ldclump
+
+
 #plink \
 #    --bfile /scratch/andreeki/clump/figi_controls_1000 \
 #    --memory 8000 \
-#    --clump ${wdir}/figi_gxescan_${exposure}_chiSqControl_ldclump.txt \
-#    --clump-p1 0.0001 --clump-r2 0.2 --clump-p2 1 \
-#    --out   ${wdir}/figi_gxescan_${exposure}_chiSqControl_ldclump
-
-
-plink \
-    --bfile /scratch/andreeki/clump/figi_controls_1000 \
-    --memory 8000 \
-    --clump ${wdir}/FIGI_${hrc_version}_gxeset_${exposure}_chiSq2df_no_gwas_ldclump.txt \
-    --clump-p1 0.00000005 --clump-r2 0.15 --clump-p2 1 \
-    --out   ${wdir}/FIGI_${hrc_version}_gxeset_${exposure}_chiSq2df_no_gwas_ldclump
-
-plink \
-    --bfile /scratch/andreeki/clump/figi_controls_1000 \
-    --memory 8000 \
-    --clump ${wdir}/FIGI_${hrc_version}_gxeset_${exposure}_chiSq3df_no_gwas_no_ge_ldclump.txt \
-    --clump-p1 0.00000005 --clump-r2 0.15 --clump-p2 1 \
-    --out   ${wdir}/FIGI_${hrc_version}_gxeset_${exposure}_chiSq3df_no_gwas_no_ge_ldclump
+#    --clump ${wdir}/FIGI_${hrc_version}_gxeset_${exposure}_chiSq2df_ldclump.txt \
+#    --clump-p1 0.00000005 --clump-r2 0.15 --clump-p2 1 \
+#    --out   ${wdir}/FIGI_${hrc_version}_gxeset_${exposure}_chiSq2df_ldclump
+#
+#plink \
+#    --bfile /scratch/andreeki/clump/figi_controls_1000 \
+#    --memory 8000 \
+#    --clump ${wdir}/FIGI_${hrc_version}_gxeset_${exposure}_chiSq3df_no_gwas_no_ge_ldclump.txt \
+#    --clump-p1 0.00000005 --clump-r2 0.15 --clump-p2 1 \
+#    --out   ${wdir}/FIGI_${hrc_version}_gxeset_${exposure}_chiSq3df_no_gwas_no_ge_ldclump
